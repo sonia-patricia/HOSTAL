e1aa7be5e5f2073430c4e7f3cf28c68034361c84
Wladi <jes.gonzalezr@alumnos.duoc.cl>
2021-06-07 13:36:38 -0400

    Creación Modelo Cliente/Avance Mantenedor Cliente

--- hostal/website/models.py
+++ hostal/website/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.urls import reverse
+import uuid
 
-# Create your models here.
+class Cliente(models.Model):
+
+    rut_cliente = models.IntegerField(primary_key=True, help_text='')
+    dv = models.CharField(max_length=1)
+    nombre = models.CharField(max_length=25)
+    telefono = models.IntegerField()
+    email = models.CharField(max_length=25)
+    direccion = models.CharField(max_length=25)
+    usuario = models.CharField(max_length=25)
+    contrasenia = models.CharField(max_length=8)
+
+    def get_absolute_url(self):
+        return reverse('cliente-detail', args=[str(self.id)])
+
+    def __str__(self):
+        return self.nombre
--- hostal/website/views.py
+++ hostal/website/views.py
@@ -1,6 +1,37 @@
 from django.shortcuts import render
 from django.http.response import HttpResponse
 
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Cliente
+
+from django.views import generic
+
+
 # Create your views here.
 def home(request):
     return render(request, 'website/home.html')
+
+def clientes(request):
+    return render(request, 'website/clientes.html')   
+     
+
+class ClienteCreate(CreateView):
+    model = Cliente
+    fields = '__all__'
+
+class ClienteUpdate(UpdateView):
+    model = Cliente
+    #fields = ['nombre','telefono','email','direccion','usuario','contrasenia']    
+    fields = '__all__'
+
+class ClienteDelete(DeleteView):
+    model = Cliente
+    success_url = reverse_lazy('clientes')
+
+class ClienteDetailView(generic.DetailView):
+    model = Cliente    
+
+class ClienteListView(generic.ListView):
+    model = Cliente
+    template_name = 'website/clientes_list.html'
